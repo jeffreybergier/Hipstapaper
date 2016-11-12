5cebd5a8cd87020f9f8737e5013c106c12e6ae48
jeffreybergier <jeffburg@jeffburg.com>
2016-11-11 20:21:31 -0800

    initial commit

---
+++ Hipstapaper/Hipstapaper/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  Hipstapaper
+//
+//  Created by Jeffrey Bergier on 11/11/16.
+//  Copyright © 2016 Jeffrey Bergier. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    let rootWindowController: URLListWindowController
+    
+    override init() {
+        let windowController = URLListWindowController(windowNibName: "URLListWindowController")
+        self.rootWindowController = windowController
+        super.init()
+    }
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        self.rootWindowController.showWindow(self)
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) { }
+}
+
