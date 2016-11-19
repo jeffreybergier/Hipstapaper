c50870479de04f14b26a85883b63ebf388d2827e
jeffreybergier <jeffburg@jeffburg.com>
2016-11-19 13:46:13 -0800

    made main window respond to open menu

--- Hipstapaper/Hipstapaper/URLListWindowController.swift
+++ Hipstapaper/Hipstapaper/URLListWindowController.swift
@@ -11,18 +11,23 @@ import AppKit
 
 class URLListWindowController: NSWindowController {
     
+    // MARK: Data Source
+    
     fileprivate let cloudKitComms = CloudKitComms(recordType: "URLItem")
     @IBOutlet private weak var uiBindingObserver: URLListBindingObserver?
     @IBOutlet private weak var tableView: NSTableView?
     
+    // MARK: Manage Child Windows of this Window
     fileprivate var openItemWindows = [URLBindingItem : URLItemWebViewWindowController]()
     var isPresentingChildWindows: Bool {
         return !self.openItemWindows.isEmpty
     }
     
+    //MARK: Initialization
+    
     convenience init() {
         self.init(windowNibName: "URLListWindowController")
-        self.window!.isExcludedFromWindowsMenu = true
+        self.window?.isExcludedFromWindowsMenu = true
     }
 
     override func windowDidLoad() {
@@ -31,25 +36,56 @@ class URLListWindowController: NSWindowController {
         self.uiBindingObserver?.delegate = self
     }
     
+    // MARK: Handle Toolbar Buttons
+    
     @IBAction private func refreshButonClicked(_ sender: NSObject?) { // IB can send anything and swift won't check unless I do.
         self.tableView?.deselectAll(self)
         self.reloadData()
     }
     
+    // MARK: Handle Table View Actions
+    
     @IBAction func tableViewDoubleClicked(_ sender: NSObject?) {
         guard let selectedItems = self.uiBindingObserver?.selectedItems else { return }
-        for item in selectedItems {
-            if let oldWC = self.openItemWindows[item] {
-                oldWC.showWindow(self)
-            } else {
-                let newWC = URLItemWebViewWindowController(urlItem: item)
-                self.openItemWindows[item] = newWC
-                NotificationCenter.default.addObserver(self, selector: #selector(self.itemWindowWillClose(_:)), name: .NSWindowWillClose, object: newWC.window!)
-                newWC.showWindow(self)
-            }
+        self.openItemWindows(for: selectedItems)
+    }
+    
+    // MARK: Handle Menu Bar Items
+    
+    override func validateMenuItem(_ menuItem: NSMenuItem) -> Bool {
+        guard menuItem.title == "Open Selected" else { fatalError() }
+        if let selectedItems = self.uiBindingObserver?.selectedItems, selectedItems.isEmpty == false {
+            return true
+        } else {
+            return false
         }
     }
     
+    @objc private func open(_ sender: NSObject?) {
+        guard let selectedItems = self.uiBindingObserver?.selectedItems else { return }
+        self.openItemWindows(for: selectedItems)
+    }
+    
+    // MARK: Open Windows based on User Input
+    
+    private func openItemWindows(for selectedItems: [URLBindingItem]) {
+        for item in selectedItems {
+            self.existingOrNewItemWindowController(for: item).showWindow(self)
+        }
+    }
+    
+    private func existingOrNewItemWindowController(for item: URLBindingItem) -> NSWindowController {
+        if let oldWC = self.openItemWindows[item] {
+            return oldWC
+        } else {
+            let newWC = URLItemWebViewWindowController(urlItem: item)
+            self.openItemWindows[item] = newWC
+            NotificationCenter.default.addObserver(self, selector: #selector(self.itemWindowWillClose(_:)), name: .NSWindowWillClose, object: newWC.window!)
+            return newWC
+        }
+    }
+    
+    // MARK: Reload Data and Update UI
     
     private func reloadData() {
         self.cloudKitComms.reloadData() { result in
@@ -65,6 +101,9 @@ class URLListWindowController: NSWindowController {
 }
 
 extension URLListWindowController: RecordChangeDelegate {
+    
+    // MARK: Observe Changes in Data from UI and forward it to CloudKit Sync
+    
     func bindingsObserver(_: URLListBindingObserver, didAdd records: [URLBindingItem]) {
         records.forEach({ self.cloudKitComms.writeToCloudKit(item: $0.record, completionHandler: {_ in}) })
     }
@@ -77,6 +116,9 @@ extension URLListWindowController: RecordChangeDelegate {
 }
 
 extension URLListWindowController /*NSWindowDelegate*/ {
+    
+    // MARK: Handle Child Window Closing to Remove from OpenItemWindows Property and from Memory
+    
     @objc fileprivate func itemWindowWillClose(_ notification: NSNotification) {
         guard
             let window = notification.object as? NSWindow,
