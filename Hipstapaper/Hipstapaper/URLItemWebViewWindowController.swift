e01c55330db714945910723717ed269d501ac208
jeffreybergier <jeffburg@jeffburg.com>
2016-11-18 18:15:37 -0800

    added webview delegates
    will probably only need the UI Delegate

--- Hipstapaper/Hipstapaper/URLItemWebViewWindowController.swift
+++ Hipstapaper/Hipstapaper/URLItemWebViewWindowController.swift
@@ -13,7 +13,9 @@ class URLItemWebViewWindowController: NSWindowController {
     
     private(set) var item: URLBindingItem?
     private let titlePrefixer = TitlePrefixer(titlePrefix: "Hipstapaper: ")
-        
+    private let webViewNavigationDelegate = WebViewNavigationDelegate()
+    private let webViewUIDelegate = WebViewUIDelegate()
+    
     private let webView: WKWebView = {
         let config = WKWebViewConfiguration()
         let webView = WKWebView(frame: CGRect.zero, configuration: config)
@@ -39,6 +41,10 @@ class URLItemWebViewWindowController: NSWindowController {
         self.window?.contentView?.trailingAnchor.constraint(equalTo: self.webView.trailingAnchor, constant: 0).isActive = true
         self.window?.contentView?.topAnchor.constraint(equalTo: self.webView.topAnchor, constant: 0).isActive = true
         self.window?.contentView?.bottomAnchor.constraint(equalTo: self.webView.bottomAnchor, constant: 0).isActive = true
+        
+        // configure webview delegates
+        self.webView.navigationDelegate = self.webViewNavigationDelegate
+        self.webView.uiDelegate = self.webViewUIDelegate
         
         // Get the URL loading
         // could probably use a bail out here if this unwrapping fails
