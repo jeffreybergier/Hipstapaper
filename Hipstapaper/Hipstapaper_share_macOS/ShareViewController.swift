9b070722be154e8a6f823b62b900d2e96bfdcc9e
jeffreybergier <jeffburg@jeffburg.com>
2016-12-01 15:14:11 -0800

    added mac share extension

---
+++ Hipstapaper/Hipstapaper_share_macOS/ShareViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ShareViewController.swift
+//  Hipstapaper_share_macOS
+//
+//  Created by Jeffrey Bergier on 12/1/16.
+//  Copyright © 2016 Jeffrey Bergier. All rights reserved.
+//
+
+import Cocoa
+
+class ShareViewController: NSViewController {
+
+    override var nibName: String? {
+        return "ShareViewController"
+    }
+
+    override func loadView() {
+        super.loadView()
+    
+        // Insert code here to customize the view
+        let item = self.extensionContext!.inputItems[0] as! NSExtensionItem
+        if let attachments = item.attachments {
+            NSLog("Attachments = %@", attachments as NSArray)
+        } else {
+            NSLog("No Attachments")
+        }
+    }
+
+    @IBAction func send(_ sender: AnyObject?) {
+        let outputItem = NSExtensionItem()
+        // Complete implementation by setting the appropriate value on the output item
+    
+        let outputItems = [outputItem]
+        self.extensionContext!.completeRequest(returningItems: outputItems, completionHandler: nil)
+}
+
+    @IBAction func cancel(_ sender: AnyObject?) {
+        let cancelError = NSError(domain: NSCocoaErrorDomain, code: NSUserCancelledError, userInfo: nil)
+        self.extensionContext!.cancelRequest(withError: cancelError)
+    }
+
+}
