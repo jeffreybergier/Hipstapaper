8022997fffdf1581c3db276ed66ea09fc04541ac
jeffreybergier <jeffburg@jeffburg.com>
2016-12-01 15:25:13 -0800

    moved persistence to its own framework

---
+++ Hipstapaper/HipstapaperPersistence_macOS/HipstapaperPersistence_macOS.h
@@ -0,0 +1,19 @@
+//
+//  HipstapaperPersistence_macOS.h
+//  HipstapaperPersistence_macOS
+//
+//  Created by Jeffrey Bergier on 12/1/16.
+//  Copyright © 2016 Jeffrey Bergier. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for HipstapaperPersistence_macOS.
+FOUNDATION_EXPORT double HipstapaperPersistence_macOSVersionNumber;
+
+//! Project version string for HipstapaperPersistence_macOS.
+FOUNDATION_EXPORT const unsigned char HipstapaperPersistence_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HipstapaperPersistence_macOS/PublicHeader.h>
+
+
