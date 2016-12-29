a186ec0613ad6cae9b2e831aabaf0e6bdf43eb52
jeffreybergier <jeffburg@jeffburg.com>
2016-12-29 14:23:21 -0800

    removed need for aspects in project

---
+++ Hipstapaper/Hipstapaper_app_iOS/PreviewActionInjectionSafariViewController.swift
@@ -0,0 +1,9 @@
+//
+//  PreviewActionInjectionSafariViewController.swift
+//  Hipstapaper
+//
+//  Created by Jeffrey Bergier on 12/29/16.
+//  Copyright © 2016 Jeffrey Bergier. All rights reserved.
+//
+
+import Foundation
