5c5130feabeee635d475e1c9cc6add16874a1838
jeffreybergier <jeffburg@jeffburg.com>
2016-12-30 10:54:43 -0800

    improved animation of share extension

--- Hipstapaper/Hipstapaper_share_iOS/URLShareiOSViewController.swift
+++ Hipstapaper/Hipstapaper_share_iOS/URLShareiOSViewController.swift
@@ -12,9 +12,17 @@ class URLShareiOSViewController: XPURLShareViewController {
     
     override var uiState: UIState {
         didSet {
-            if self.waitingTimer == nil {
-                self.waitingTimerFired(.none)
-                self.waitingTimer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(self.waitingTimerFired(_:)), userInfo: .none, repeats: false)
+            DispatchQueue.main.async {
+                switch self.uiState {
+                case .start:
+                    self.messageLabel?.text = "Saving  📱"
+                case .saving:
+                    self.messageLabel?.text = "Saving  📱"
+                case .error:
+                    self.messageLabel?.text = "Error  😭"
+                case .saved:
+                    self.messageLabel?.text = "Saved  ☺️"
+                }
             }
         }
     }
@@ -31,51 +39,45 @@ class URLShareiOSViewController: XPURLShareViewController {
         }
     }
     
-    private var waitingTimer: Timer?
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.containerViewVerticalSpaceConstraint?.constant = UIScreen.main.bounds.height
         self.view.layoutIfNeeded()
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        UIView.animate(withDuration: 0.5, delay: 0.0, options: [.curveEaseInOut], animations: {
+            self.modalView?.alpha = 0.3
+            self.containerViewVerticalSpaceConstraint?.constant = UIScreen.main.bounds.height / 2.5
+            self.view.layoutIfNeeded()
+        }, completion: { success in
+            Timer.scheduledTimer(timeInterval: 1.5, target: self, selector: #selector(self.waitingTimerFired(_:)), userInfo: .none, repeats: true)
+        })
+    }
+    
     @objc private func waitingTimerFired(_ timer: Timer?) {
-        timer?.invalidate()
-        self.waitingTimer?.invalidate()
-        self.waitingTimer = .none
         let uiState = self.uiState
-        DispatchQueue.main.async {
-            UIView.animate(withDuration: 0.5, delay: 0.0, options: [.curveEaseInOut], animations: {
-                switch uiState {
-                case .start:
-                    self.modalView?.alpha = 0.0
-                    self.containerViewVerticalSpaceConstraint?.constant = UIScreen.main.bounds.height
-                    self.messageLabel?.text = "Saving  📱"
-                case .saving:
-                    self.modalView?.alpha = 0.3
-                    self.containerViewVerticalSpaceConstraint?.constant = UIScreen.main.bounds.height / 2.5
-                    self.messageLabel?.text = "Saving  📱"
-                case .error:
-                    self.modalView?.alpha = 0.0
-                    self.containerViewVerticalSpaceConstraint?.constant = UIScreen.main.bounds.height / 2.5
-                    self.messageLabel?.text = "Error  😭"
-                case .saved:
-                    self.modalView?.alpha = 0.0
-                    self.containerViewVerticalSpaceConstraint?.constant = -50
-                    self.messageLabel?.text = "Saved  ☺️"
-                }
-                self.view.layoutIfNeeded()
-            }, completion: { success in
-                switch uiState {
-                case .saved:
-                    self.extensionContext?.completeRequest(returningItems: .none, completionHandler: { _ in })
-                case .error:
-                    self.extensionContext?.cancelRequest(withError: NSError(domain: "", code: 0, userInfo: nil))
-                default:
-                    break
-                }
-            })
-        }
+        guard uiState == .saved || uiState == .error else { return }
+        
+        timer?.invalidate()
+        
+        UIView.animate(withDuration: 0.5, delay: 0.0, options: [.curveEaseInOut], animations: {
+            
+            self.modalView?.alpha = 0.0
+            self.containerViewVerticalSpaceConstraint?.constant = -50
+            self.view.layoutIfNeeded()
+            
+        }, completion: { sucess in
+            
+            if case .saved = uiState {
+                self.extensionContext?.completeRequest(returningItems: .none, completionHandler: { _ in })
+            } else if case .error = uiState {
+                self.extensionContext?.cancelRequest(withError: NSError(domain: "", code: 0, userInfo: nil))
+            }
+            
+        })
     }
 }
 
