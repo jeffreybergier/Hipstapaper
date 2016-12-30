d721ce74db358523be7caf85b3bf7365ca9d7f0e
jeffreybergier <jeffburg@jeffburg.com>
2016-12-30 10:54:31 -0800

    processing always calls back on main thread now

--- Hipstapaper/Hipstapaper_realm/SaveExtensionFileProcessor.swift
+++ Hipstapaper/Hipstapaper_realm/SaveExtensionFileProcessor.swift
@@ -16,46 +16,46 @@ public class SaveExtensionFileProcessor {
     private var processingInProgress = false
     
     public func processFiles(with realmController: RealmController?, completionHandler: ((XPBackgroundFetchResult) -> Void)? = nil) {
-        guard self.processingInProgress == false else {
-            completionHandler?(.failed)
-            return
-        }
-        self.processingInProgress = true
-        DispatchQueue.global(qos: .background).async {
-            guard let realmController = realmController else {
+        DispatchQueue.main.async {
+            guard self.processingInProgress == false, let realmController = realmController else {
                 completionHandler?(.failed)
                 self.processingInProgress = false
                 return
             }
-            guard let itemsOnDisk = NSKeyedUnarchiver.unarchiveObject(withFile: SerializableURLItem.archiveURL.path) as? [SerializableURLItem] else {
-                // delete the file if it exists and has incorrect data, or else this could fail forever and never get fixed
-                try? FileManager.default.removeItem(at: SerializableURLItem.archiveURL)
-                completionHandler?(.noData)
-                self.processingInProgress = false
-                return
-            }
-            DispatchQueue.main.async {
-                for item in itemsOnDisk {
-                    guard let urlString = item.urlString else { continue }
-                    let newURLItem = URLItem()
-                    newURLItem.urlString = urlString
-                    newURLItem.creationDate = item.date ?? newURLItem.creationDate
-                    newURLItem.modificationDate = item.date ?? newURLItem.modificationDate
-                    let newExtras = URLItemExtras()
-                    newExtras.image = item.image
-                    newExtras.pageTitle = item.pageTitle
-                    if newExtras.pageTitle != nil || newExtras.imageData != nil {
-                        newURLItem.extras = newExtras
+            self.processingInProgress = true
+            DispatchQueue.global(qos: .background).async {
+                guard let itemsOnDisk = NSKeyedUnarchiver.unarchiveObject(withFile: SerializableURLItem.archiveURL.path) as? [SerializableURLItem] else {
+                    // delete the file if it exists and has incorrect data, or else this could fail forever and never get fixed
+                    try? FileManager.default.removeItem(at: SerializableURLItem.archiveURL)
+                    DispatchQueue.main.async {
+                        completionHandler?(.noData)
+                        self.processingInProgress = false
                     }
-                    realmController.add(item: newURLItem)
+                    return
                 }
-                try? FileManager.default.removeItem(at: SerializableURLItem.archiveURL)
-                if itemsOnDisk.isEmpty == false {
-                    completionHandler?(.newData)
-                } else {
-                    completionHandler?(.noData)
+                DispatchQueue.main.async {
+                    for item in itemsOnDisk {
+                        guard let urlString = item.urlString else { continue }
+                        let newURLItem = URLItem()
+                        newURLItem.urlString = urlString
+                        newURLItem.creationDate = item.date ?? newURLItem.creationDate
+                        newURLItem.modificationDate = item.date ?? newURLItem.modificationDate
+                        let newExtras = URLItemExtras()
+                        newExtras.image = item.image
+                        newExtras.pageTitle = item.pageTitle
+                        if newExtras.pageTitle != nil || newExtras.imageData != nil {
+                            newURLItem.extras = newExtras
+                        }
+                        realmController.add(item: newURLItem)
+                    }
+                    try? FileManager.default.removeItem(at: SerializableURLItem.archiveURL)
+                    if itemsOnDisk.isEmpty == false {
+                        completionHandler?(.newData)
+                    } else {
+                        completionHandler?(.noData)
+                    }
+                    self.processingInProgress = false
                 }
-                self.processingInProgress = false
             }
         }
     }
