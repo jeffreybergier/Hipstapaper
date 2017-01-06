832c89394503e7709bd6b94bd1cea438d67e0cfe
jeffreybergier <jeffburg@jeffburg.com>
2017-01-06 09:36:33 -0800

    started adding selection to the dropdowns

--- Hipstapaper/Hipstapaper_app_macOS/SortSelectingViewController.swift
+++ Hipstapaper/Hipstapaper_app_macOS/SortSelectingViewController.swift
@@ -10,10 +10,28 @@ import Cocoa
 
 class SortSelectingViewController: NSViewController {
 
+    @IBOutlet private weak var sortOrderPopupButton: NSPopUpButton?
+    @IBOutlet private weak var filterPopupButton: NSPopUpButton?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        print("Selecting View Controller Loaded")
     }
     
+    @IBAction private func sortOrderChosen(_ sender: NSObject?) {
+        guard let button = sender as? NSPopUpButton, button === self.sortOrderPopupButton, let selection = SortOrderSelection(rawValue: button.selectedItem?.tag ?? 55) else { return }
+        print("Sort Order Chosen: \(selection)")
+    }
+    
+    @IBAction func filterChosen(_ sender: NSObject?) {
+        guard let button = sender as? NSPopUpButton, button === self.filterPopupButton, let selection = FilterSelection(rawValue: button.selectedItem?.tag ?? 55) else { return }
+        print("Sort Order Chosen: \(selection)")
+    }
+}
+
+enum FilterSelection: Int {
+    case unarchived = 0, all
+}
+
+enum SortOrderSelection: Int {
+    case recentlyAddedOnTop = 2, recentlyAddedOnBottom = 3, recentlyModifiedOnTop = 4, recentlyModifiedOnBottom = 5, urlAOnTop = 6, urlZOnTop = 7
 }
