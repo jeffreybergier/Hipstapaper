43a20e14275aee698aeafc6fdc305b10755c74ee
jeffreybergier <jeffburg@jeffburg.com>
2017-01-14 11:04:25 -0800

    added better extension item processing

--- Hipstapaper/Hipstapaper_realm/SerializableURLItem.swift
+++ Hipstapaper/Hipstapaper_realm/SerializableURLItem.swift
@@ -81,9 +81,10 @@ extension SerializableURLItem {
         
         // since this is all async, we need to make sure we return only when the object is full
         // hitcount goes from 0 to 2, because there are 3 properties of this object
+        let itemCount = extensionItem.attachments?.count ?? 0
         var hitCount = 0 {
             didSet {
-                guard hitCount == 2 else { return }
+                guard hitCount == itemCount * 2 else { return }
                 DispatchQueue.main.async {
                     // our object is valid
                     if let urlString = outputItem.urlString, let _ = URL(string: urlString) {
@@ -97,37 +98,42 @@ extension SerializableURLItem {
         
         // make sure we have an attachment
         // if not, return nil
-        guard let attachment = extensionItem.attachments?.first as? NSItemProvider else { handler(.error); return; }
-        
-        // load the URL for the object
-        attachment.loadItem(forTypeIdentifier: kUTTypeURL as String, options: .none) { secureCoding, error in
-            //https://developer.apple.com/library/content/documentation/Miscellaneous/Reference/UTIRef/Articles/System-DeclaredUniformTypeIdentifiers.html
-            // public.url (kUTTypeURL) public.data 'url'   Uniform Resource Locator.
-            //      this one finds File and Web Page URLs
-            // public.file-url (kUTTypeFileURL)    public.url  'furl'  File URL.
-            //      this one finds only File URLs and not web pages
-            // public.url-name -   'urln'  URL name.
-            //      not sure what this one does, it didn't find either
+        guard let attachments = extensionItem.attachments as? [NSItemProvider] else { handler(.error); return; }
+        for attachment in attachments.reversed() {
+            // load the URL for the object
+            attachment.loadItem(forTypeIdentifier: kUTTypeURL as String, options: .none) { secureCoding, error in
+                //https://developer.apple.com/library/content/documentation/Miscellaneous/Reference/UTIRef/Articles/System-DeclaredUniformTypeIdentifiers.html
+                // public.url (kUTTypeURL) public.data 'url'   Uniform Resource Locator.
+                //      this one finds File and Web Page URLs
+                // public.file-url (kUTTypeFileURL)    public.url  'furl'  File URL.
+                //      this one finds only File URLs and not web pages
+                // public.url-name -   'urln'  URL name.
+                //      not sure what this one does, it didn't find either
+                
+                let url = secureCoding as? URL
+                if let urlString = url?.absoluteString {
+                    outputItem.urlString = urlString
+                }
+                hitCount += 1
+            }
             
-            let url = secureCoding as? URL
-            outputItem.urlString = url?.absoluteString
-            hitCount += 1
-        }
-        
-        // load a preview image for the object
-        #if os(OSX)
-            let desiredImageSize = NSValue(size: NSSize(width: 1024, height: 1024))
-        #else
-            let desiredImageSize = NSValue(cgSize: CGSize(width: 1024, height: 768))
-        #endif
-        attachment.loadPreviewImage(options: [NSItemProviderPreferredImageSizeKey : desiredImageSize]) { secureCoding, error in
+            // load a preview image for the object
             #if os(OSX)
-                let discoveredImage = secureCoding as? NSImage
+                let desiredImageSize = NSValue(size: NSSize(width: 512, height: 512))
             #else
-                let discoveredImage = secureCoding as? UIImage
+                let desiredImageSize = NSValue(cgSize: CGSize(width: 512, height: 512))
             #endif
-            outputItem.image = discoveredImage
-            hitCount += 1
+            attachment.loadPreviewImage(options: [NSItemProviderPreferredImageSizeKey : desiredImageSize]) { secureCoding, error in
+                #if os(OSX)
+                    let discoveredImage = secureCoding as? NSImage
+                #else
+                    let discoveredImage = secureCoding as? UIImage
+                #endif
+                if let discoveredImage = discoveredImage {
+                    outputItem.image = discoveredImage
+                }
+                hitCount += 1
+            }
         }
     }
 }
