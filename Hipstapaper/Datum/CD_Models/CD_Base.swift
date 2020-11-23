733a98ca9d44731e26d38906963bbe72267a66a7
Jeffrey Bergier <jeffburg@jeffburg.com>
2020-11-23 22:47:29 +0900

    added CD_Base model file

---
+++ Hipstapaper/Datum/CD_Models/CD_Base.swift
@@ -0,0 +1,46 @@
+//
+//  Created by Jeffrey Bergier on 2020/11/23.
+//
+//  Copyright © 2020 Saturday Apps.
+//
+//  This file is part of Hipstapaper.
+//
+//  Hipstapaper is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Hipstapaper is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with Hipstapaper.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+import CoreData
+
+@objc internal class CD_Base: NSManagedObject {
+
+    /// Template for fetch request in subclasses
+    private class var entityName: String { "CD_Base" }
+    private class var request: NSFetchRequest<CD_Base> {
+        NSFetchRequest<CD_Base>(entityName: self.entityName)
+    }
+
+    @NSManaged internal var dateCreated: Date
+    @NSManaged internal var dateModified: Date
+
+    override internal func awakeFromInsert() {
+        super.awakeFromInsert()
+        let date = Date()
+        self.dateModified = date
+        self.dateCreated = date
+    }
+
+    /// Override to validate your properties before saving
+    internal func performPropertyValidation() {
+        self.dateModified = Date()
+    }
+}
