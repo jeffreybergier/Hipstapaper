8a9d3e6190e114331fb20ee3a4096e07276fbff3
Jeffrey Bergier <jeffburg@jeffburg.com>
2020-11-23 22:14:37 +0900

    added datum framework

---
+++ Hipstapaper/Datum/Datum.h
@@ -0,0 +1,18 @@
+//
+//  Datum.h
+//  Datum
+//
+//  Created by Jeffrey Bergier on 2020/11/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Datum.
+FOUNDATION_EXPORT double DatumVersionNumber;
+
+//! Project version string for Datum.
+FOUNDATION_EXPORT const unsigned char DatumVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Datum/PublicHeader.h>
+
+
