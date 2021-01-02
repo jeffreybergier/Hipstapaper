8edd9cc3c40246000b413c04d9c52c281ffb8f4a
jeffreybergier <jeffburg@jeffburg.com>
2021-01-02 22:23:14 +0900

    added separators in regular toolbar

--- Hipstapaper/Shared/Main/Toolbars/DetailToolbar_iOS.swift
+++ Hipstapaper/Shared/Main/Toolbars/DetailToolbar_iOS.swift
@@ -40,16 +40,16 @@ struct DetailToolbar_iOS: ViewModifier {
     
     func body(content: Content) -> some View {
         return self.isCompact
-        ? AnyView(
-            content.modifier(DetailToolbar_Compact_iOS(controller: self.controller,
-                                                       presentation: self.$presentation,
-                                                       popoverAlignment: self.$popoverAlignment))
-        )
-        : AnyView(
-            content.modifier(DetailToolbar_Regular_iOS(controller: self.controller,
-                                                       presentation: self.$presentation,
-                                                       popoverAlignment: self.$popoverAlignment))
-        )
+            ? AnyView(
+                content.modifier(DetailToolbar_Compact_iOS(controller: self.controller,
+                                                           presentation: self.$presentation,
+                                                           popoverAlignment: self.$popoverAlignment))
+            )
+            : AnyView(
+                content.modifier(DetailToolbar_Regular_iOS(controller: self.controller,
+                                                           presentation: self.$presentation,
+                                                           popoverAlignment: self.$popoverAlignment))
+            )
     }
 }
 
@@ -63,85 +63,97 @@ fileprivate struct DetailToolbar_Regular_iOS: ViewModifier {
     @EnvironmentObject var windowManager: WindowManager
     
     func body(content: Content) -> some View {
-        content.toolbar(id: "Detail_iOS_Regular") {
-            //
-            // Bottom Buttons
-            //
-            ToolbarItem(id: "Detail_iOS_Regular.Share", placement: .bottomBar) {
-                DT.Share(isDisabled: self.controller.selectedWebsites.isEmpty)
-                {
-                    self.popoverAlignment = .bottomLeading
-                    self.presentation.value = .share
+        content
+            .toolbar(id: "Detail_iOS_Regular") {
+                //
+                // Bottom Buttons
+                //
+                ToolbarItem(id: "Detail_iOS_Regular.Archive", placement: .bottomBar) {
+                    DT.Unarchive(isDisabled: self.controller.selectedWebsites.filter { !$0.value.isArchived }.isEmpty)
+                    {
+                        // Archive
+                        let selected = self.controller.selectedWebsites
+                        self.controller.selectedWebsites = []
+                        try! self.controller.controller.update(selected, .init(isArchived: true)).get()
+                    }
                 }
-            }
-            ToolbarItem(id: "Detail_iOS_Regular.Tag", placement: .bottomBar) {
-                DT.Tag(isDisabled: self.controller.selectedWebsites.isEmpty)
-                {
-                    self.popoverAlignment = .bottomLeading
-                    self.presentation.value = .tagApply
+                ToolbarItem(id: "Detail_iOS_Regular.Unarchive", placement: .bottomBar) {
+                    DT.Unarchive(isDisabled: self.controller.selectedWebsites.filter { $0.value.isArchived }.isEmpty)
+                    {
+                        // Unarchive
+                        let selected = self.controller.selectedWebsites
+                        self.controller.selectedWebsites = []
+                        try! self.controller.controller.update(selected, .init(isArchived: false)).get()
+                    }
                 }
-            }
-            ToolbarItem(id: "Detail_iOS_Regular.Archive", placement: .bottomBar) {
-                DT.Unarchive(isDisabled: self.controller.selectedWebsites.filter { !$0.value.isArchived }.isEmpty)
-                {
-                    // Archive
-                    let selected = self.controller.selectedWebsites
-                    self.controller.selectedWebsites = []
-                    try! self.controller.controller.update(selected, .init(isArchived: true)).get()
+                ToolbarItem(id: "Detail_iOS_Regular.FilterSeparator", placement: .bottomBar) {
+                    ButtonToolbarSeparator()
                 }
-            }
-            ToolbarItem(id: "Detail_iOS_Regular.Unarchive", placement: .bottomBar) {
-                DT.Unarchive(isDisabled: self.controller.selectedWebsites.filter { $0.value.isArchived }.isEmpty)
-                {
-                    // Unarchive
-                    let selected = self.controller.selectedWebsites
-                    self.controller.selectedWebsites = []
-                    try! self.controller.controller.update(selected, .init(isArchived: false)).get()
+                ToolbarItem(id: "Detail_iOS_Regular.Tag", placement: .bottomBar) {
+                    DT.Tag(isDisabled: self.controller.selectedWebsites.isEmpty)
+                    {
+                        self.popoverAlignment = .bottomLeading
+                        self.presentation.value = .tagApply
+                    }
                 }
-            }
-            ToolbarItem(id: "Detail_iOS_Regular.OpenInApp", placement: .bottomBar) {
-                DT.OpenInApp(selectionCount: self.controller.selectedWebsites.count) {
-                    guard self.windowManager.features.contains([.multipleWindows, .bulkActivation])
-                    else { self.presentation.value = .browser; return }
-                    let urls = self.controller.selectedWebsites.compactMap
-                    { $0.value.resolvedURL ?? $0.value.originalURL }
-                    self.windowManager.show(urls) {
-                        // TODO: Do something with this error
-                        print($0)
+                ToolbarItem(id: "Detail_iOS_Regular.FilterSeparator", placement: .bottomBar) {
+                    Spacer()
+                }
+                ToolbarItem(id: "Detail_iOS_Regular.OpenInApp", placement: .bottomBar) {
+                    DT.OpenInApp(selectionCount: self.controller.selectedWebsites.count) {
+                        guard self.windowManager.features.contains([.multipleWindows, .bulkActivation])
+                        else { self.presentation.value = .browser; return }
+                        let urls = self.controller.selectedWebsites.compactMap
+                        { $0.value.resolvedURL ?? $0.value.originalURL }
+                        self.windowManager.show(urls) {
+                            // TODO: Do something with this error
+                            print($0)
+                        }
+                    }
+                }
+                ToolbarItem(id: "Detail_iOS_Regular.OpenExternal", placement: .bottomBar) {
+                    DT.OpenExternal(selectionCount: self.controller.selectedWebsites.count) {
+                        let urls = self.controller.selectedWebsites
+                            .compactMap { $0.value.resolvedURL ?? $0.value.originalURL }
+                        urls.forEach { self.openURL($0) }
+                    }
+                }
+                ToolbarItem(id: "Detail_iOS_Regular.FilterSeparator", placement: .bottomBar) {
+                    ButtonToolbarSeparator()
+                }
+                ToolbarItem(id: "Detail_iOS_Regular.Share", placement: .bottomBar) {
+                    DT.Share(isDisabled: self.controller.selectedWebsites.isEmpty)
+                    {
+                        self.popoverAlignment = .bottomTrailing
+                        self.presentation.value = .share
                     }
                 }
             }
-            ToolbarItem(id: "Detail_iOS_Regular.OpenExternal", placement: .bottomBar) {
-                DT.OpenExternal(selectionCount: self.controller.selectedWebsites.count) {
-                    let urls = self.controller.selectedWebsites
-                        .compactMap { $0.value.resolvedURL ?? $0.value.originalURL }
-                    urls.forEach { self.openURL($0) }
+            .toolbar(id: "Detail_iOS_Regular") {
+                
+                //
+                // Top Bar Items
+                //
+                
+                ToolbarItem(id: "Detail_iOS_Regular.Filter", placement: .automatic) {
+                    DT.Filter(filter: self.controller.query.isArchived) {
+                        self.popoverAlignment = .topTrailing
+                        self.controller.query.isArchived.toggle()
+                    }
+                }
+                ToolbarItem(id: "Detail_iOS_Regular.Sort", placement: .automatic) {
+                    ButtonToolbarSort {
+                        self.popoverAlignment = .topTrailing
+                        self.presentation.value = .sort
+                    }
+                }
+                ToolbarItem(id: "Detail_iOS_Regular.Search", placement: .primaryAction) {
+                    DT.Search(searchActive: self.controller.query.search.nonEmptyString != nil) {
+                        self.popoverAlignment = .topTrailing
+                        self.presentation.value = .search
+                    }
                 }
             }
-            
-            //
-            // Top Bar Items
-            //
-            
-            ToolbarItem(id: "Detail_iOS_Regular.Filter", placement: .automatic) {
-                DT.Filter(filter: self.controller.query.isArchived) {
-                    self.popoverAlignment = .topTrailing
-                    self.controller.query.isArchived.toggle()
-                }
-            }
-            ToolbarItem(id: "Detail_iOS_Regular.Sort", placement: .automatic) {
-                ButtonToolbarSort {
-                    self.popoverAlignment = .topTrailing
-                    self.presentation.value = .sort
-                }
-            }
-            ToolbarItem(id: "Detail_iOS_Regular.Search", placement: .primaryAction) {
-                DT.Search(searchActive: self.controller.query.search.nonEmptyString != nil) {
-                    self.popoverAlignment = .topTrailing
-                    self.presentation.value = .search
-                }
-            }
-        }
     }
 }
 
