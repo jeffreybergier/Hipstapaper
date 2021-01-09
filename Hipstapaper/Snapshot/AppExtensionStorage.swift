53eb71f3d7c94d25db1c86eceae2847c145641bf
jeffreybergier <jeffburg@jeffburg.com>
2021-01-09 22:57:49 +0900

    added directory publisher class

--- Hipstapaper/Snapshot/AppExtensionStorage.swift
+++ Hipstapaper/Snapshot/AppExtensionStorage.swift
@@ -20,3 +20,62 @@
 //
 
 import Foundation
+import Combine
+
+public enum AppGroup {
+    public static let container: URL = {
+        let fm = FileManager.default
+        #if os(macOS)
+        let id = "V6ESYGU6CV.com.saturdayapps.Hipstapaper"
+        #else
+        let id = "group.com.saturdayapps.Hipstapaper"
+        #endif
+        return fm.containerURL(forSecurityApplicationGroupIdentifier: id)!
+    }()
+}
+
+open class DirectoryPresenter: NSObject, NSFilePresenter {
+    open var presentedItemOperationQueue = OperationQueue.main
+    open var presentedItemURL: URL?
+    open func presentedSubitemDidChange(at url: URL) { }
+    public init(url: URL) {
+        self.presentedItemURL = url
+    }
+}
+
+open class DirectoryPublisher: DirectoryPresenter {
+    
+    private var _publisher = PassthroughSubject<URL, Never>()
+    open lazy var publisher: AnyPublisher<URL, Never> = {
+        var setupPerformed = false
+        return AnyPublisher(_publisher.handleEvents(receiveSubscription:
+        { [unowned self] subs in
+            guard !setupPerformed else { return }
+            setupPerformed = true
+            self.setupOnce()
+        }))
+    }()
+    
+    open func setupOnce() {
+        let fm = FileManager.default
+        let existingContents = try? fm.contentsOfDirectory(
+            at: self.presentedItemURL!,
+            includingPropertiesForKeys: [.isRegularFileKey, .isHiddenKey],
+            options: [.skipsHiddenFiles, .skipsPackageDescendants, .skipsSubdirectoryDescendants]
+        )
+        NSFileCoordinator.addFilePresenter(self)
+        for url in existingContents ?? [] {
+            let check = try? url.resourceValues(forKeys: [.isRegularFileKey, .isHiddenKey])
+            guard check?.isRegularFile == true && check?.isHidden == false else { return }
+            self.presentedItemOperationQueue.underlyingQueue!.async {
+                self._publisher.send(url)
+            }
+        }
+    }
+    
+    public override func presentedSubitemDidChange(at url: URL) {
+        let check = try? url.resourceValues(forKeys: [.isRegularFileKey, .isHiddenKey])
+        guard check?.isRegularFile == true && check?.isHidden == false else { return }
+        _publisher.send(url)
+    }
+}
