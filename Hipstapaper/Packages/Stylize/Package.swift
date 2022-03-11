c446f2dbba2cd6ebbe37b9574dd21f2d2c745b80
jeffreybergier <jeffburg@jeffburg.com>
2022-03-11 18:26:10 +0900

    SPM: Stylize

--- Hipstapaper/Packages/Stylize/Package.swift
+++ Hipstapaper/Packages/Stylize/Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "Stylize",
+    platforms: [.iOS(.v13), .macOS(.v11)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -12,15 +13,20 @@ let package = Package(
             targets: ["Stylize"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(path: "../Localize"),
+        .package(url: "https://github.com/jeffreybergier/XPList.git", .branchItem("main")),
+        .package(url: "https://github.com/jeffreybergier/Umbrella.git", .branchItem("main"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Stylize",
-            dependencies: []),
+            dependencies: [
+                .byNameItem(name: "Localize", condition: nil),
+                .byNameItem(name: "XPList", condition: nil),
+                .byNameItem(name: "Umbrella", condition: nil),
+            ]),
         .testTarget(
             name: "StylizeTests",
             dependencies: ["Stylize"]),
