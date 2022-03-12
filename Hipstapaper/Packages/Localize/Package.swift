91a3ad4b1486a29d261c5d47e9a42f2cc809e51c
jeffreybergier <jeffburg@jeffburg.com>
2022-03-12 10:24:31 +0900

    cleaned up unneeded tests

--- Hipstapaper/Packages/Localize/Package.swift
+++ Hipstapaper/Packages/Localize/Package.swift
@@ -1,5 +1,30 @@
 // swift-tools-version:5.5
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+//
+//  Created by Jeffrey Bergier on 2022/03/11.
+//
+//  MIT License
+//
+//  Copyright (c) 2021 Jeffrey Bergier
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+//
 
 import PackageDescription
 
@@ -7,23 +32,18 @@ let package = Package(
     name: "Localize",
     platforms: [.iOS(.v13), .macOS(.v11)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "Localize",
-            targets: ["Localize"]),
+            targets: ["Localize"]
+        ),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+    
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Localize",
-            dependencies: []),
-        .testTarget(
-            name: "LocalizeTests",
-            dependencies: ["Localize"]),
+            dependencies: []
+        ),
     ]
 )
