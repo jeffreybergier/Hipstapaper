590ea125e537c8c8db34bdf3875d214263468606
jeffreybergier <jeffburg@jeffburg.com>
2022-03-12 10:17:48 +0900

    SPM: fixed localization

--- Hipstapaper/Packages/Localize/Package.swift
+++ Hipstapaper/Packages/Localize/Package.swift
@@ -5,7 +5,6 @@ import PackageDescription
 
 let package = Package(
     name: "Localize",
-    defaultLocalization: "en-US",
     platforms: [.iOS(.v13), .macOS(.v11)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
