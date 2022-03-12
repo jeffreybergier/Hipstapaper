9f0d1b0ca352ee2e0b3641613e48f3c645ae5f4c
jeffreybergier <jeffburg@jeffburg.com>
2022-03-12 10:32:30 +0900

    SPM: fixed testing schemes

--- Hipstapaper/Packages/Interface/Package.swift
+++ Hipstapaper/Packages/Interface/Package.swift
@@ -1,5 +1,30 @@
 // swift-tools-version:5.5
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+//
+//  Created by Jeffrey Bergier on 2022/03/11.
+//
+//  MIT License
+//
+//  Copyright (c) 2021 Jeffrey Bergier
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+//
 
 import PackageDescription
 
@@ -7,10 +32,10 @@ let package = Package(
     name: "Interface",
     platforms: [.iOS(.v14), .macOS(.v11)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "Interface",
-            targets: ["Interface"]),
+            targets: ["Interface"]
+        ),
     ],
     dependencies: [
         .package(path: "../Datum"),
@@ -18,8 +43,6 @@ let package = Package(
         .package(path: "../Snapshot"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Interface",
             dependencies: [
@@ -28,8 +51,5 @@ let package = Package(
                 .byNameItem(name: "Snapshot", condition: nil),
             ]
         ),
-        .testTarget(
-            name: "InterfaceTests",
-            dependencies: ["Interface"]),
     ]
 )
