e1408944f656959235bbd12580bc78e58bc7b4f4
jeffreybergier <jeffburg@jeffburg.com>
2022-06-17 16:46:21 +0900

    added temporary app target

---
+++ Hipstapaper/Hipstapaper/HipstapaperApp.swift
@@ -0,0 +1,17 @@
+//
+//  HipstapaperApp.swift
+//  Hipstapaper
+//
+//  Created by Jeffrey Bergier on 2022/06/17.
+//
+
+import SwiftUI
+
+@main
+struct HipstapaperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
