7d98491bbae852d1819b876616460a8ba8e626cd
jeffreybergier <jeffburg@jeffburg.com>
2022-08-03 20:41:02 +0900

    move files around

--- Hipstapaper/Packages/V3Interface/Sources/V3Interface/Main/ShareList/ShareListRow.swift
+++ Hipstapaper/Packages/V3Interface/Sources/V3Interface/Main/ShareList/ShareListRow.swift
@@ -31,62 +31,6 @@ import V3Store
 import V3Style
 import V3Localize
 
-internal struct ShareList: View {
-    
-    @Controller private var controller
-    @State private var allItems: [URL] = []
-    
-    @V3Style.ShareList private var style
-    @V3Localize.ShareList private var text
-    
-    @Environment(\.dismiss) private var dismiss
-        
-    private let selection: Website.Selection
-    private let selectionA: Array<Website.Selection.Element>
-    
-    internal init(_ selection: Website.Selection) {
-        self.selection = selection
-        self.selectionA = Array(selection)
-    }
-    
-    internal var body: some View {
-        NavigationStack {
-            Form {
-                self.allItems.view { urls in
-                    ShareLink(items: urls) {
-                        self.style.shareLabel(icon: .multi) {
-                            Text(self.text.shareAll)
-                        } subtitle: {
-                            Text(self.text.itemsCount(urls.count))
-                        }
-                    }
-                } onEmpty: {
-                    self.style.shareLabel(icon: .error) {
-                        Text(self.text.shareAll)
-                    } subtitle: {
-                        Text(self.text.shareError)
-                    }
-                }
-                ForEach(self.selectionA) { identifier in
-                    ShareListRow(identifier)
-                }
-            }
-            .modifier(self.toolbar)
-        }
-        .frame(idealWidth: 320, idealHeight: 320)
-        .onLoadChange(of: self.selection) {
-            let result = BulkActionsQuery.openURL($0, self.controller)
-            self.allItems = result.map { Array($0.multi) } ?? []
-        }
-    }
-    
-    private var toolbar: some ViewModifier {
-        JSBToolbar(title: self.text.title,
-                   done: self.text.done,
-                   doneAction: self.dismiss.callAsFunction)
-    }
-}
-
 internal struct ShareListRow: View {
     
     @WebsiteQuery private var item
@@ -124,18 +68,5 @@ internal struct ShareListRow: View {
     // does not work on chained optionals
     private var itemURL: URL? {
         self.item?.preferredURL
-    }
-}
-
-internal struct ShareListPresentation: ViewModifier {
-    
-    @Nav private var nav
-
-    internal func body(content: Content) -> some View {
-        content.popover(items: self.$nav.detail.isShare)
-        { selection in
-            ShareList(selection)
-                .presentationDetents([.medium])
-        }
     }
 }
