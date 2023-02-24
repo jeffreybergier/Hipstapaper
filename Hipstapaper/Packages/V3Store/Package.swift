014d8b659f3d717acbdd04e5dd0d5afc2cabfe59
jeffreybergier <jeffburg@jeffburg.com>
2023-02-24 20:56:03 +0900

    [misc] switched to umbrella main branch

--- Hipstapaper/Packages/V3Store/Package.swift
+++ Hipstapaper/Packages/V3Store/Package.swift
@@ -39,7 +39,7 @@ let package = Package(
     ],
     dependencies: [
         .package(path: "../V3Model"),
-        .package(url: "https://github.com/jeffreybergier/Umbrella.git", branch: "waterme3"),
+        .package(url: "https://github.com/jeffreybergier/Umbrella.git", branch: "main"),
     ],
     targets: [
         .target(
