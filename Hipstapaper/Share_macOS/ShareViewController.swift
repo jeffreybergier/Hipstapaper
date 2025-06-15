fe1b7ea4a295aa33d1a317fc00d2125861672943
Wamalwa <155254269+waabiud@users.noreply.github.com>
2025-06-15 19:58:54 +0300

    Update ShareViewController.swift

--- Hipstapaper/Share_macOS/ShareViewController.swift
+++ Hipstapaper/Share_macOS/ShareViewController.swift
@@ -1,95 +1,70 @@
-//
-//  Created by Jeffrey Bergier on 2021/01/08.
-//
-//  MIT License
-//
-//  Copyright (c) 2021 Jeffrey Bergier
-//
-//  Permission is hereby granted, free of charge, to any person obtaining a copy
-//  of this software and associated documentation files (the "Software"), to deal
-//  in the Software without restriction, including without limitation the rights
-//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-//  copies of the Software, and to permit persons to whom the Software is
-//  furnished to do so, subject to the following conditions:
-//
-//  The above copyright notice and this permission notice shall be included in all
-//  copies or substantial portions of the Software.
-//
-//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-//  SOFTWARE.
-//
-
-import AppKit
+import Cocoa
 import SwiftUI
-import V3WebsiteEdit
+import UniformTypeIdentifiers
 
 class ShareViewController: NSViewController {
     
-    private var inputURL: URL?
-    private lazy var shareUIVC: NSViewController = {
-        let view = ShareExtension(inputURL: self.inputURL)
-        { [weak extensionContext] in
-            extensionContext?.completeRequest(returningItems: nil,
-                                              completionHandler: nil)
-        }
-        return NSHostingController(rootView: view)
-    }()
-    
+    override func loadView() {
+        self.view = NSView()
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        guard let context = self.extensionContext?.inputItems.first as? NSExtensionItem else {
-            self.configureVC()
-            assertionFailure("Could not get extension context")
-            return
-        }
-        context.urlValue() { url in
-            self.inputURL = url
-            self.configureVC()
-        }
-    }
-    
-    private func configureVC() {
-        let vc = self.shareUIVC
-        vc.view.translatesAutoresizingMaskIntoConstraints = false
-        self.view.addSubview(vc.view)
-        self.view.addConstraints([
-            self.view.topAnchor.constraint(equalTo: vc.view.topAnchor, constant: 0),
-            self.view.bottomAnchor.constraint(equalTo: vc.view.bottomAnchor, constant: 0),
-            self.view.leadingAnchor.constraint(equalTo: vc.view.leadingAnchor, constant: 0),
-            self.view.trailingAnchor.constraint(equalTo: vc.view.trailingAnchor, constant: 0)
+        let inputItem = extensionContext?.inputItems.first as? NSExtensionItem
+        let url = inputItem?.urlValue() ?? inputItem?.plainTextValue()
+        
+        let sharedContent = url?.absoluteString ?? "No valid content shared."
+        let childView = NSHostingController(rootView: ShareExtension(sharedText: sharedContent))
+
+        addChild(childView)
+        view.addSubview(childView.view)
+
+        childView.view.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            childView.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            childView.view.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            childView.view.topAnchor.constraint(equalTo: view.topAnchor),
+            childView.view.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
-        self.addChild(vc)
+    }
+
+    override func viewDidAppear() {
+        super.viewDidAppear()
+        NSApp.activate(ignoringOtherApps: true)
+    }
+
+    func completeExtensionRequest() {
+        extensionContext?.completeRequest(returningItems: [], completionHandler: nil)
     }
 }
 
-import UniformTypeIdentifiers
+private extension NSExtensionItem {
+    func urlValue() -> URL? {
+        attachments?.compactMap { $0 as? NSItemProvider }
+            .first(where: { $0.hasItemConformingToTypeIdentifier(UTType.url.identifier) })?
+            .loadItemAndCast(as: URL.self)
+    }
 
-extension NSExtensionItem {
-    fileprivate func urlValue(completion: @escaping (URL?) -> Void) {
-        let contentType = UTType.url.identifier
-        let _a = self.attachments?.first(where: { $0.hasItemConformingToTypeIdentifier(contentType) })
-        guard let attachment = _a else {
-            completion(nil)
-            return
+    func plainTextValue() -> URL? {
+        attachments?.compactMap { $0 as? NSItemProvider }
+            .first(where: { $0.hasItemConformingToTypeIdentifier(UTType.plainText.identifier) })?
+            .loadItemAndCast(as: String.self)
+            .flatMap { URL(string: $0) } // attempt to parse as URL if it’s in text form
+    }
+}
+
+private extension NSItemProvider {
+    func loadItemAndCast<T>(as type: T.Type) -> T? {
+        var result: T?
+        let semaphore = DispatchSemaphore(value: 0)
+
+        loadItem(forTypeIdentifier: registeredTypeIdentifiers.first ?? "", options: nil) { item, _ in
+            result = item as? T
+            semaphore.signal()
         }
-        attachment.loadItem(forTypeIdentifier: contentType, options: nil) { data, _ in
-            DispatchQueue.main.async {
-                guard
-                    let data = data as? Data,
-                    let urlString = String(data: data, encoding: .utf8),
-                    let url = URL(string: urlString)
-                else {
-                    completion(nil)
-                    return
-                }
-                completion(url)
-            }
-        }
+
+        semaphore.wait()
+        return result
     }
 }
